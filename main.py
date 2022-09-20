b332e39eb01b592bcb36e96aa143a257b85796ae
nadeem-24 <nknadeem800@gmail.com>
2022-09-20 06:18:34 +0530

    Code correction

--- main.py
+++ main.py
@@ -37,27 +37,3 @@ updater.dispatcher.add_handler(CommandHandler('command', command))
 updater.start_polling()
 
 
-if __name__ == "__main":
-
-    updater = Updater(api.BOT_API, use_context=True)
-
-    def start(update: Update, context: CallbackContext):
-        update.message.reply_text(
-            '''Welcome to BSE_infobot.Please type /commands to know valild list of commands.''')
-
-    def gainers(update: Update, context: CallbackContext):
-        update.message.reply_text(bse.Data.top_gainers())
-
-    def losers(update: Update, context: CallbackContext):
-        update.message.reply_text(bse.Data.top_losers())
-
-    def command(update: Update, context: CallbackContext):
-        update.message.reply_text('''/gainers -> get a list of today's top gainers
-        /losers -> get a list of today's top losers''')
-
-    updater.dispatcher.add_handler(CommandHandler('start', start))
-    updater.dispatcher.add_handler(CommandHandler('gainers', gainers))
-    updater.dispatcher.add_handler(CommandHandler('losers', losers))
-    updater.dispatcher.add_handler(CommandHandler('command', command))
-
-    updater.start_polling()
